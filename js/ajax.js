eab41e21b5175764876141e446dd8c87d5d20912
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2025-06-03 15:49:07 +0200

    new files

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+function request(callback,api,method){
+
+    // Create XML HTTP request.
+ let xhr = new XMLHttpRequest();
+ xhr.open(method,api);
+ xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+
+ // Add onload function.
+ xhr.onload = () => {
+ switch (xhr.status) {
+ case 200:
+ case 201: callback(JSON.parse(xhr.responseText));
+ break;
+default: console.log('HTTP error: ' + xhr.status);
+ }
+ };
+
+ // Send XML HTTP request.
+ xhr.send();
+}
